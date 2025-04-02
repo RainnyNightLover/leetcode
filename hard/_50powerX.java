2f574c8107882028c9c2cef8da85a7c2d9afb3f0
yangxing <yangxing@tsingj.com>
2025-04-02 13:53:19 +0800

    add _50powerX class with initial implementation

---
+++ hard/_50powerX.java
@@ -0,0 +1,20 @@
+package hard;
+
+/**
+ * 思考问题无法解答的思路：
+ * 1、溢出问题解决
+ *
+ */
+public class _50powerX {
+    public int lengthOfLongestSubstring(String s) {
+
+        return 1;
+    }
+
+
+    public static void main(String[] args) {
+        _50powerX _03lengthOfLongestSubstring = new _50powerX();
+        System.out.println(_03lengthOfLongestSubstring.lengthOfLongestSubstring("bbbb"));
+    }
+
+}
